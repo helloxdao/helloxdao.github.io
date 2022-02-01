b665bdc00660cc689f25a75511597c6a5c6bb91d
Carlos IP <42459213+helloxdao@users.noreply.github.com>
2022-02-02 01:06:51 +0800

    Add files via upload

---
+++ precache-manifest.dbc9e967edb2ea872d14bb3eda461e8b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "79370a31e93b4c3cbbc8",
+    "url": "/js/fr_FR.34a49cee.js"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "666590ebe3bae4d4b00b",
+    "url": "/js/zh_CN.6f2e1d57.js"
+  },
+  {
+    "revision": "f30910030b0bf63e00ed",
+    "url": "/js/ja_JP.1731c3d8.js"
+  },
+  {
+    "revision": "665e04ccc7ad08a302ab",
+    "url": "/js/it_IT.3e777d89.js"
+  },
+  {
+    "revision": "865ad769fb614cf88973",
+    "url": "/js/es_MX.f13f825e.js"
+  },
+  {
+    "revision": "8ef09545e3b819417f06",
+    "url": "/js/en_US.716cb16d.js"
+  },
+  {
+    "revision": "f261836637c9766870a7",
+    "url": "/js/de_DE.074c81f3.js"
+  },
+  {
+    "revision": "20d643f317ed007c64ae",
+    "url": "/js/chunk-vendors.16bc1f70.js"
+  },
+  {
+    "revision": "8d53e26e7772f40b9eb8",
+    "url": "/js/app.2431d406.js"
+  },
+  {
+    "revision": "0c55a1af458db7f4a0876f876966a789",
+    "url": "/007icu.html"
+  }
+];
